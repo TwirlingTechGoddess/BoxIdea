cf434bb3fb15fb3d757d70acc46e0c1f0c5eaa72
Lee Graham <graham.la3@gmail.com>
2018-04-05 13:06:17 -0600

    Refactor for accessibility and upvote downvote value persist

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ $inputBody.keyup(toggleButton);
 $submitButton.on('click', createIdea);
 $('main').on('click', '.delete-button', deleteIdea);
 $('main').on('click', $upVote, upVoteQuality);
-// $('main').on('click', $downVote, downVoteQuality);
+$('main').on('click', $downVote, downVoteQuality);
 // $('main').on('change', $outputTitle, editTexts);
 // $('main').on('change', $outputBody, editTexts);
 $searchBar.keyup(searchIdeas);
@@ -42,30 +42,23 @@ function createIdea(event) {
   title = $inputTitle.val();
   body = $inputBody.val();
   var newIdea = new BoxIdea(title, body);
+  debugger;
   prependCards(newIdea);
   storeMyIdea(newIdea);
 }
-  
+
+
 function prependCards(newIdea) {
-  var swillOptions = `<option value="swill" selected>swill</option>
-          <option value="plausible">plausible</option>
-          <option value="genius">genius</option>`
-
-
-
   $('.info-from-inputs').prepend(`
     <article id=${newIdea.id}>
-      <h2 class="output-title" contenteditable="true">${newIdea.title}</h2>
-      <button class="delete-button"></button>
+      <h2 class="output-title" contenteditable="true" aria-label="editable-idea-title">${newIdea.title}</h2>
+      <button class="delete-button" name="Delete-Button" aria-label="button-to-delete-this-idea"></button>
       <br>
-      <p class="output-body" contenteditable="true">${newIdea.body}</p>
+      <p class="output-body" contenteditable="true" aria-label="editable-idea-body">${newIdea.body}</p>
       <form>
-        <button class="upvote-button"></button>
-        <button class="downvote-button"></button>
-        <h3>quality:</h3>
-        <select class="quality-selector" name="quality">\
-          ${swillOptions}
-        </select>
+        <button class="upvote-button" aria-label="Up-Vote-Button"></button>
+        <button class="downvote-button" aria-label="Down-Vote-Button"></button>
+        <h3>quality: ${newIdea.quality}</h3>
       </form>
     </article>
   `);
@@ -99,43 +92,39 @@ function deleteIdea(event) {
   var keysOmine = $(event.target).parent('article').attr("id");
   localStorage.removeItem(keysOmine); 
   $(event.target).parent('article').remove();
-};
-
-// upVoteQuality();
+}
 
 function upVoteQuality(event) {
   event.preventDefault();
   var capturedId = $(event.target).parent().parent().attr("id");
   var pullStorage = localStorage.getItem(capturedId);
   var parsedStorage = JSON.parse(pullStorage);
-  var qualityBox = $(event.target).siblings('select').children('option');
-  if (qualityBox[0].selected === true) {
-    qualityBox[0].selected = false;
-    qualityBox[1].selected = true;
-    debugger;
-    parsedStorage.quality = "plausible";
-  } else {
-    qualityBox[2].selected = true;
-    parsedStorage.quality = "genius";
+  if (parsedStorage.quality === 'swill') {
+    parsedStorage.quality = 'plausible';
+  } else if (parsedStorage.quality === 'plausible') {
+    parsedStorage.quality = 'genius';
   } 
   stringifyQuality = JSON.stringify(parsedStorage);
   localStorage.setItem(capturedId, stringifyQuality);
+  prependCards(parsedStorage);
+  window.location.reload(true);
 }
 
-// function downVoteQuality(event) {
-//   event.preventDefault();
-//   $(event.target) === $downVote;
-//   var capturedId = $(event.target).parent().parent().attr("id");
-//   var qualityBox = $(event.target).siblings('select');
-//   var selectBubble = $qualityBox.children('option');
-//   if (selectBubble[2].selected === true) {
-//     selectBubble[2].selected = false;
-//     selectBubble[1].selected = true;
-//   } else if($selectBubble[1].selected === true) {
-//     selectBubble[1].selected = false;
-//     selectBubble[0].selected = true;
-//   }
-// }
+function downVoteQuality(event) {
+  event.preventDefault();
+  var capturedId = $(event.target).parent().parent().attr("id");
+  var pullStorage = localStorage.getItem(capturedId);
+  var parsedStorage = JSON.parse(pullStorage);
+  if (parsedStorage.quality === 'genius') {
+    parsedStorage.quality = 'plausible';
+  } else if (parsedStorage.quality === 'plausible') {
+    parsedStorage.quality = 'swill';
+  } 
+  stringifyQuality = JSON.stringify(parsedStorage);
+  localStorage.setItem(capturedId, stringifyQuality);
+  prependCards(parsedStorage);
+  window.location.reload();
+}
 
 // function editTexts() {
 //   event.preventDefault();
